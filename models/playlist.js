fbaeeab6f37e57ff64b12466c6188e154f39ea4f
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-08-28 22:51:51 -0400

    merged files

---
+++ models/playlist.js
@@ -0,0 +1,33 @@
+//Use the sequelize constructor to design a model for each new playlist and create SQL data 
+module.exports = function(sequelize, DataTypes) {
+
+    var Playlist = sequelize.define("Playlist", {
+        playlist_id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        playlist_name: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                len: { args: [1,50], msg: "String length is not in range" }
+            }
+        },
+        createdAt: {
+            type: DataTypes.DATE(3), 
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)')
+        },
+        updatedAt: {
+            type: DataTypes.DATE(3), 
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)')
+        }
+    },
+    {
+        tableName: 'Playlists'
+    });
+  
+    return Playlist;
+  };
