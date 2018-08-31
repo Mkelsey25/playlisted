44578c3041891822977a6e6c2c250cec4b3e45cd
homsau <james.p.cowart@gmail.com>
2018-08-30 23:17:01 -0400

    songs.js and schema corrections

--- models/songs.js
+++ models/songs.js
@@ -39,9 +39,8 @@ module.exports = function(sequelize, DataTypes) {
         },
         energy: {
             type: DataTypes.ENUM,
-            values: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            values: ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'],
             allowNull: false,
-            defaultValue: 'Unknown',
             validate: {
                 len: [1]
             }
