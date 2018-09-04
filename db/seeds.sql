16d039194c0354e315df3dfc298b56e15bbc0593
Mkelsey <mkh13b@my.fsu.edu>
2018-09-04 14:55:47 -0400

    added Indie songs to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,7 +16,21 @@ VALUES ("Come Sail Away", "Styx",  "02/02/02", "Ecstatic", "0.9", "Rock", CURREN
        ("Waves of Galveston", "Iron and Wine", "01/01/01", "Sad", "0.2", "Easy Listening", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
        ("London Hymn", "Josh Groban", "10/10/10", "Meh", "0.3", "Classical", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
        ("Ave Maria", "Josh Groban", "10/10/10", "Meh", "0.3", "Classical", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
-       ("Everybody's Changing", "Keane", "11/11/11", "Angry", "0.7", "Easy Listening", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3));
+       ("Everybody's Changing", "", "11/11/11", "Angry", "0.7", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Yam Yam", "No Vacation", "01/01/2017", "Happy", "0.7", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Deep Sea Dive", "Snail Mail", "06/08/2018", "Sad", "0.4", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Space Song", "Beach House", "01/01/2015", "Meh", "0.5", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Flaming Hot Cheetos", "Clairo", "01/01/2018", "Happy", "0.6", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("When the Night is Over", "Lord Heron", "04/01/2018", "Meh", "0.5", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Marry Me, Archie", "Alvvays", "01/01/2014", "Happy", "0.7", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Mistake", "Middle Kids", "01/01/2018", "Angry", "0.8", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Edge of Town", "Middle Kids", "01/01/2018", "Meh", "0.6", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Smother", "Daughter", "01/01/2013", "Sad", "0.3", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Weak", "WET", "01/01/2016", "Sad", "0.3", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("Don't Wanna be Your Girl", "WET", "01/01/2016", "Sad", "0.3", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+       ("One of These Days", "Bedouine", "Easy Listening", "Happy", "0.2", "Indie", CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3)),
+
+       ;
 
 
 select * from songs;
