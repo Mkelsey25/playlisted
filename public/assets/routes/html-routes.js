03303f93534302d086cbcf07b6140aadc5d0fbb0
homsau <james.p.cowart@gmail.com>
2018-09-05 21:03:56 -0400

    push playlist to index.handlebars, added css, modified some

--- public/assets/routes/html-routes.js
+++ public/assets/routes/html-routes.js
@@ -18,25 +18,6 @@ module.exports = function(app) {
     res.render("index", {
       title: "Playlisted"
     });
-<<<<<<< HEAD
-=======
-
-    /*  get render of songs to index page to work
-        algorithm for choosing which songs to add to playlist
-        variable to display songs */
-
-    /*var query = {};
-    db.Songs.findAll({
-      where: query
-    }).then(function(dbResult) {
-
-      // send to handlebars
-      var hbsObject = {
-        songs: dbResult
-      };
-      res.render("index", hbsObject);
-    });*/
->>>>>>> a04ccf8edd216e4e76ea0e72ca252274066d9daa
   });
 
   // login route loads login.html
